a946eac00b07ab0b631f024cd9e9819866831566
Charles GERARD-LE METAYER <contact@glm-system.fr>
2025-08-10 13:37:01 +0200

    - Import manquant - Correction logique enregistrement

--- internal/services/subscription/subscription_resource.go
+++ internal/services/subscription/subscription_resource.go
@@ -28,6 +28,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/resourceproviders"
 )
 
 var SubscriptionResourceName = "azurerm_subscription"
